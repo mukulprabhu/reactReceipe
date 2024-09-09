be22ac547c6e50e1403b91abe33f76fbeefcd3e6
Mohit Prabhu <mukulprabhu@gmail.com>
2024-09-09 16:39:32 -0500

    Initial commin

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import FoodList from "./components/FoodList";
+import Search from "./components/Search";
+import React, { useState } from "react";
+
+import "./App.css";
+import Nav from "./components/Nav";
+import Container from "./components/Container";
+import InnerContainer from "./components/InnerContainer";
+import FoodDetails from "./components/FoodDetails";
+
+function App() {
+  const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState();
+  return (
+    <div>
+      <Nav />
+      <Search foodData={foodData} setFoodData={setFoodData} />
+      <Container>
+        <InnerContainer>
+          <FoodList foodData={foodData} setFoodId={setFoodId} />
+        </InnerContainer>
+        <InnerContainer>
+          <FoodDetails foodId={foodId} />
+        </InnerContainer>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
